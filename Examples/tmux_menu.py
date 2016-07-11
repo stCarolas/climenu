8824c7e193167aef372c19d37ac2b9ee2755feaf
stCarolas <stcarolas@gmail.com>
2016-07-11 16:17:53 +0300

    add tmux menu

--- Examples/tmux_menu.py
+++ Examples/tmux_menu.py
@@ -1,2 +1,20 @@
 #!/usr/bin/env python3
-print('{"menu":[{"name": "test1","action": "vim ."}]}')
+import subprocess
+if __name__ == '__main__':
+    processResult = subprocess.run(["tmux", "list-session"], stdout = subprocess.PIPE)
+    stdout = processResult.stdout.decode("utf-8")
+    sessions = stdout.split("\n")
+
+    names = []
+    for session in sessions:
+        name = session.split(":")[0]
+        if (name != None and len(name) >0):
+            names.append(name)
+
+    menu = []
+    for name in names:
+        menuItem = dict()
+        menuItem['name'] = name
+        menuItem['action'] = "vim ."
+        menu.append(menuItem)
+    print(menu)
