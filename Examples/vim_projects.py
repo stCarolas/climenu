11019579e61771becb122024718f5a2138586da6
stCarolas <stcarolas@gmail.com>
2016-07-17 17:02:33 +0300

    add vim projects menu

---
+++ Examples/vim_projects.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import json
+import copy
+from os import walk, listdir
+from os.path import expanduser
+from pathlib import Path
+
+def check_dir(path):
+    rootPath = str(path)
+    gradleFilePath = Path(rootPath).joinpath("build.gradle")
+    if gradleFilePath.exists():
+        return True
+
+def get_menu():
+    projects_dir_path = "~/Coding/projects"
+    p = Path(expanduser(projects_dir_path))
+
+    subdirs = []
+    for subdir in p.iterdir():
+        if subdir.is_dir() and check_dir(subdir):
+            subdirs.append(subdir)
+
+    menuItems = []
+    for subdir in subdirs:
+        menuItem = dict()
+        menuItem['name'] = subdir.name
+        menuItem['action'] = "vim " + str(subdir.resolve())
+        menuItems.append(menuItem)
+    menu = {"menu":menuItems}
+    return menu
+
+if __name__ == '__main__':
+    print(json.dumps(get_menu()))
--- Makefile
+++ Makefile
@@ -3,11 +3,13 @@ all: install_to_home
 install:
 	cp m1.py /usr/local/bin/m1
 	cp Examples/tmux_menu.py /home/stcarolas/.config/m1/generators
+	cp Examples/vim_projects.py /home/stcarolas/.config/m1/generators
 	cp Examples/menu /home/stcarolas/.config/m1/
 	
 
 install_to_home:
 	cp m1.py ~/.local/bin/m1
 	cp Examples/tmux_menu.py ~/.config/m1/generators
+	cp Examples/vim_projects.py ~/.config/m1/generators
 	cp Examples/menu ~/.config/m1/
 
